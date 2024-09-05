4fcd508df286bca51d20a00d310fc13cfb5dab2b
migueldr12 <110634700+migueldr12@users.noreply.github.com>
2024-09-04 23:39:15 -0600

    Initial commit

---
+++ src/main/java/com/mycompany/curp704/Curp704.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.curp704;
+
+/**
+ *
+ * @author migue
+ */
+public class Curp704 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
